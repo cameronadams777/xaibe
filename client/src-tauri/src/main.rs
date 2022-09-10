807de3c97ab7bbb6f01d7e49fb84ab443584f168
Cameron Adams <cameron.adams@hey.com>
2022-09-10 03:10:26 -0500

    feat(client): intialize Tauri code

---
+++ client/src-tauri/src/main.rs
@@ -0,0 +1,14 @@
+#![cfg_attr(
+  all(not(debug_assertions), target_os = "windows"),
+  windows_subsystem = "windows"
+)]
+
+fn main() {
+  tauri::Builder::default()
+    .invoke_handler(tauri::generate_handler![get_messages])
+    .run(tauri::generate_context!())
+    .expect("error while running tauri application");
+}
+
+#[tauri::command]
+fn get_messages() {}
