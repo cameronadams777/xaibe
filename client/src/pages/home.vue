f34d8292ea136bb9ed2ff36f8ef79596c05ce996
Cameron Adams <cameron.adams@hey.com>
2022-10-25 23:18:29 -0500

    chore(client): cleanup home api calls

--- client/src/pages/home.vue
+++ client/src/pages/home.vue
@@ -1,23 +1,12 @@
 <script lang="ts" setup>
 import TheMainLayout from "../layouts/the-main-layout.vue";
-import NoAlerts from "../components/no-alerts.vue";
-import { useAlertsStore } from "../state/alerts";
 import { useActiveUserStore } from "../state/active-user";
 import { onMounted } from "vue";
-import { storeToRefs } from "pinia";
 
-const { getActiveUser, ...activeUserStore } = useActiveUserStore();
-const { getAlerts, ...alertsStore } = useAlertsStore();
-
-const { activeUser } = storeToRefs(activeUserStore);
-const { alerts } = storeToRefs(alertsStore);
-
-// Get all existing alerts in the redis queue
-// Connect via websocket to the api and await new alerts
+const { getActiveUser } = useActiveUserStore();
 
 onMounted(async () => {
   await getActiveUser();
-  await getAlerts({ applicationId: 1, serviceToken: "" });
 });
 </script>
 
