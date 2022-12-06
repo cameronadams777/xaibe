0af84a7f9aa31d18ba4312391b9a9d8553178236
Cameron Adams <cameron.adams@hey.com>
2022-12-05 22:28:34 -0600

    chore: make redis connection dsn configurable based on envs

--- api/initializers/database/connect.go
+++ api/initializers/database/connect.go
@@ -1,6 +1,7 @@
 package database
 
 import (
+  "api/config"
 	"api/models"
 	"fmt"
 
@@ -11,7 +12,13 @@ import (
 var DB *gorm.DB
 
 func ConnectDB() {
-	dsn := "host=localhost user=postgres password=5rPwU1=x9tUHyT=jiC+KL0ht dbname=galataapp_db port=40300"
+  host := config.Get("REDIS_HOST")
+  user := config.Get("REDIS_USER")
+  password := config.Get("REDIS_PASSWORD")
+  dbname := config.Get("REDIS_DB")
+  port := config.Get("REDIS_PORT")
+
+	dsn := "host=" + host + " user=" + user + " password=" + password + " dbname=" + dbname + " port=" + port
 	database, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 
 	if err != nil {
