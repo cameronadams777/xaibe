2b113849b38df81879334bab32c43a6f90059fe3
Cameron Adams <cameron.adams@hey.com>
2023-02-04 17:19:24 -0600

    chore: refactor mixpanel env usage to utilize config module

--- client/src/config/index.ts
+++ client/src/config/index.ts
@@ -1,16 +1,19 @@
 import { z } from "zod";
 
-const apiBaseUrl = import.meta.env.VITE_API_BASE_URL;
-const apiWSUrl = import.meta.env.VITE_API_WS_URL;
-
 const configSchema = z.object({
   apiBaseUrl: z.string(),
-  apiWSUrl: z.string()
+  apiWSUrl: z.string(),
+  mixpanelToken: z.string(),
+  mixpanelDebug: z.boolean(),
+  stripePublishableKey: z.string(),
 });
 
 export const config: z.infer<typeof configSchema> = configSchema.parse({
-  apiBaseUrl,
-  apiWSUrl
+  apiBaseUrl: import.meta.env.VITE_API_BASE_URL,
+  apiWSUrl: import.meta.env.VITE_API_WS_URL,
+  mixpanelToken: import.meta.env.VITE_MIXPANEL_TOKEN,
+  mixpanelDebug: import.meta.env.VITE_MIXPANEL_DEBUG === "true",
+  stripePublishableKey: import.meta.env.VITE_STRIPE_PUBLISHABLE_KEY,
 });
 
 
--- client/src/tools/mixpanel.ts
+++ client/src/tools/mixpanel.ts
@@ -1,20 +1,18 @@
 import mixpanel, { Mixpanel } from "mixpanel-browser";
+import { config } from "src/config"; 
 
 interface IMixpanelWrapper {
   setup: () => void;
   client: Mixpanel;
 }
 
-const token = import.meta.env.VITE_MIXPANEL_TOKEN;
-const debug = import.meta.env.VITE_MIXPANEL_DEBUG;
-
 export const mixpanelWrapper: IMixpanelWrapper = {
   setup(): void {
-    if (!token.length) {
+    if (!config.mixpanelToken) { 
       console.log("Warning: No mixpanel token detected.")
       return;
     }
-    mixpanel.init(token, { debug });
+    mixpanel.init(config.mixpanelToken, { debug: config.mixpanelDebug });
     mixpanel.track("Application opened")
   },
   client: mixpanel,
