08f688a6abedc5abcbf960084075f7debd73e122
Cameron Adams <cameron.adams@hey.com>
2023-04-29 09:11:43 -0500

    wip(client): update TeamSubscriptionDetailsModal to allow editing number of seats

---
+++ client/src/components/PlusMinusNumberInput.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="w-full md:w-7/8 lg:w-3/4 flex justify-center items-center">
+    <button class="w-8 h-8 rounded-l border-none text-white font-bold bg-indigo-500 hover:bg-indigo-700 cursor-pointer" @click="decrement">-</button>
+    <input type="number" :value="numToUpdate" class="w-full m-0 text-lg text-center border-gray-200 border-t border-b border-l-none border-r-none" />
+    <button class="w-8 h-8 rounded-r border-none text-white font-bold bg-indigo-500 hover:bg-indigo-700 cursor-pointer" @click="increment">+</button>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { toRefs } from 'vue';
+
+
+const props = defineProps<{ value: number }>();
+
+const { value: numToUpdate } = toRefs(props);
+
+const emits = defineEmits<{(event: "onUpdate", value: number): void}>();
+
+const increment = () => {
+  const newVal = numToUpdate.value + 1;
+  emits("onUpdate", newVal);
+};
+
+const decrement = () => {
+  const newVal = numToUpdate.value - 1;
+  emits("onUpdate", newVal);
+};
+
+</script>
+
+<style scoped>
+/* Chrome, Safari, Edge, Opera */
+input::-webkit-outer-spin-button,
+input::-webkit-inner-spin-button {
+  -webkit-appearance: none;
+  margin: 0;
+}
+</style>
--- client/src/components/TeamSubscriptionDetailsModal.vue
+++ client/src/components/TeamSubscriptionDetailsModal.vue
@@ -1,38 +1,49 @@
 <template>
   <BaseModal :is-open="isOpen" class="w-7/12 h-3/4">
-    <div class="relative h-full p-16">
+    <div class="relative h-full p-4 md:p-16 lg:p-4 flex flex-col">
       <BaseFabButton class="absolute top-2 right-2" @click="close">
         <XMarkIcon class="w-8 h-8" />
       </BaseFabButton>
-      <h2 class="text-2xl">
+      <h2 class="text-2xl m-0 p-0">
         {{team?.name}}
       </h2>
-      <p><b>Number of Seats:</b> {{team?.activeNumberOfSeats}}</p>
+      <div class="flex flex-col items-center">
+        <p><b>Number of Seats:</b></p>
+        <PlusMinusNumberInput :value="updatedNumberOfSeats" @on-update="updateSeats" />
+      </div>
+      <BaseButton :disabled="!hasUpdates" text="Update" class="w-1/2 mt-10 self-center" />
     </div>
   </BaseModal>
 </template>
 
 <script lang="ts" setup>
-import { onMounted } from "vue";
+import { computed, onMounted, ref } from "vue";
 import { useRouter } from "vue-router";
 import { XMarkIcon } from "@heroicons/vue/24/outline";
 import { Team } from "src/types";
 import { emptyTeamSubscriptionDetailsProps, useModalStore } from "src/state";
+import PlusMinusNumberInput from "src/components/PlusMinusNumberInput.vue";
 
 const props = defineProps<{
   isOpen: boolean;
   team?: Team;
 }>();
 
+const updatedNumberOfSeats = ref<number>(0);
+const hasUpdates = computed<boolean>(() => updatedNumberOfSeats.value !== props.team?.activeNumberOfSeats);
+
 const router = useRouter();
 const { setTeamSubscriptionDetailsProps } = useModalStore();
 
+const updateSeats = (value: number) => updatedNumberOfSeats.value = value;
 const close = () => setTeamSubscriptionDetailsProps(emptyTeamSubscriptionDetailsProps); 
 
 onMounted(() => {
   if (!props.team) {
     router.push("/500");
     close();
+    return;
   }
+  updatedNumberOfSeats.value = props.team.activeNumberOfSeats;
 });
 </script>
